d8009ae671f0391deb4fc8f8a8cc3ca7975bff00
Vinicio :) <vinicioolivindo92@gmail.com>
2024-08-09 13:05:44 -0300

    att

--- aulas-Rocketseat/exercicios/ex01/script.js
+++ aulas-Rocketseat/exercicios/ex01/script.js
@@ -1,13 +1,14 @@
 const numberOne = Number(prompt('Primeiro número: '))
 const numberTwo = Number(prompt('Segundo número: '))
+const sum = numberOne + numberTwo
 
-alert(`A soma dos dois números é ${numberOne + numberTwo}`)
+alert(`A soma dos dois números é ${sum}`)
 alert(`A subtração dos dois números é ${numberOne - numberTwo}`)
 alert(`A multiplicação dos dois números é ${numberOne * numberTwo}`)
 alert(`A divisão  dos dois números é ${numberOne / numberTwo}`)
 alert(`A resto da divisão  dos dois números é ${numberOne % numberTwo}`)
 
-const sumParity = ((numberOne + numberTwo) % 2 === 0) ? "par" : "ímpar";
+const sumParity = (sum % 2 === 0) ? "par" : "ímpar";
 alert(`A soma dos dois números é ${sumParity}`);
 
 const numberComparison = (numberOne === numberTwo) ? "iguais" : "diferentes";
